03e8b70be27f3eef7a3776b76be571083022d0d4
bube009 <alexfournier09@gmail.com>
2025-04-19 15:51:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-fetch('/frequencies.json')
-  .then(response => response.json())
-  .then(data => {
-    const container = document.getElementById('freqContainer');
-    data.forEach(freq => {
-      const li = document.createElement('li');
-      li.textContent = freq.nom + ' – ' + freq.frequence + ' Hz';
-      li.onclick = () => {
-        console.log('Lecture de ' + freq.frequence + ' Hz');
-      };
-      container.appendChild(li);
-    });
-  });
+
